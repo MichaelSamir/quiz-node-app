60ead78caf67faeb2ff2fdc506895509f6afb31a
MichaelSamir <michael.samir.jacob@gmail.com>
2019-01-25 12:58:56 +0200

    add test case

--- test/1-quizzes.test.js
+++ test/1-quizzes.test.js
@@ -1,6 +1,9 @@
 //During the test the env variable is set to test
 process.env.NODE_ENV = 'test';
 
+//Set the JWT secret key
+process.env.JWT_KEY = 'secret';
+
 const Quiz = require('../api/models/quiz');
 const server = require('../server');
 var token;//user jwt token
@@ -28,7 +31,7 @@ describe('POST /quizzes', () => {
         .post('/quizzes')
         .set('Authorization', 'bearer '+ token)
         .send({
-            title: 'English Quiz',
+            title: 'English Quiz 1',
             single_choice: [{
                 question: "What ... your name?",
                 choices: [{ value: "is" }, { value: "are" }, { value: "was" }],
@@ -76,6 +79,74 @@ describe('GET /quizzes', () => {
             // the first object in the quizzes array should
             // have the right keys
             res.body.quizzes[0].should.include.keys('_id', 'creator','title');
+            done();
+        });
+    });
+});
+
+describe('GET /quizzes/:quizId', () => {
+
+    beforeEach((done) => {
+        //Empty db before test
+        Quiz.deleteMany({}, (err) => { 
+           done();           
+        });        
+    });
+
+    it('should respond with the quiz of the provided id', (done) => {
+        //Create a quiz
+        const quiz = new Quiz({
+            title: 'English Quiz 2',
+            single_choice: [{
+                question: "How many apples ... there?",
+                choices: [{ value: "is" }, { value: "doing" }, { value: "are" }],
+                correct_ans: "are"
+            }],
+            checkbox: [{
+                question: "... goes to school",
+                choices: [{ value: "He" }, { value: "Ali" }, { value: "They" }],
+                correct_ans: [{ value: "He" }, { value: "Ali" }]
+            }],
+            short_answer: { question: "Describe the weather now"}
+        });
+        quiz.save()
+        .then( quiz => {
+            chai.request(server)
+            .get('/quizzes/' + quiz._id )
+            .end((err, res) => {
+                // there should be no errors
+                should.not.exist(err);
+                // there should be a 200 status code
+                res.status.should.equal(200);
+                // the response should be JSON
+                res.type.should.equal('application/json');
+                // the JSON response body should have a
+                // key-value pair of {"quiz": [ 1 quiz object ]}
+                res.body.quiz.length.should.eql(1);
+                // the object in the quiz array should
+                // have the right keys
+                res.body.quiz[0].should.include.keys(
+                    '_id', 'title', 'creator','single_choice', 'checkbox', 'short_answer'
+                );
+                // the _id of object in the quiz array should
+                //equal quiz._id
+                res.body.quiz[0]._id.should.eql(quiz._id);
+                // the title of object in the quiz array should
+                //equal 'English Test 2'
+                res.body.quiz[0].title.should.eql('English Test 2');
+                // the single_choice of object in the quiz array should
+                //be key-value pair of {"single_choice": [ 1 question object ]}
+                res.body.quiz[0].single_choice.length.should.eql(1);
+                // the checkbox of object in the quiz array should
+                //be key-value pair of {"checkbox": [ 1 question object ]}
+                res.body.quiz[0].checkbox.length.should.eql(1);
+                // the short_answer of object in the quiz array should
+                //be key-value pair of {"short_answer": [ 1 question object ]}
+                res.body.quiz[0].short_answer.length.should.eql(1);
+                done();
+            });
+        })
+        .catch(err => {
             done();
         });
     });
